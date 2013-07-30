50cb8d4c4e38f9c832b2d291d5dd51613b465a0a
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2013-07-30 10:02:26 -0300

    Release .64

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from setuptools import setup
 
 setup(name='oerpenv',
-      version='1.7.63',
+      version='1.7.64',
       author='Cristian S. Rocha',
       author_email='cristian.rocha@moldeo.coop',
       maintainer='Cristian S. Rocha',
