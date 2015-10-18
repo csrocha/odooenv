f298fed9c130469e803174f449d41140cae3dea3
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2015-10-18 10:06:43 -0300

    [FIX] Check self.server before use it.

--- odooenv/server.py
+++ odooenv/server.py
@@ -14,22 +14,44 @@ class OdooServer:
         Arguments:
         config_filename -- Full path to the server configuration file.
         """
-        self.server = oerplib.OERP(server=server, protocol='xmlrpc', port=port)
-        if name:
-            self.user = self.server.login(user, password, name)
+        self.db = name
+        self.host = server
+        self.port = port
+        self.user = user
+        self.password = password
+        self.server = None
+
+    def login(self):
+        try:
+            self.server = oerplib.OERP(
+                    server=self.server,
+                    protocol='xmlrpc',
+                    port=self.port)
+            if self.db:
+                self.user_id = self.server.login(self.user, self.password, self.db)
+            return True
+        except:
+            return False
 
     def db_list(self):
         """
         Query for the server database list.
         """
-        self.server = oerplib.OERP(server=self.server, protocol=self.protocol, port=self.port)
-        return oerp.db.list()
+        if self.server:
+            self.server = oerplib.OERP(server=self.server, protocol=self.protocol, port=self.port)
+            return oerp.db.list()
+        else:
+            return None
 
     def update_module_list(self):
         """
         Update module list.
         """
-        mod_obj = self.server.get('ir.module.module')
-        mod_obj.update_list()
+        if self.server:
+            mod_obj = self.server.get('ir.module.module')
+            mod_obj.update_list()
+            return True
+        else:
+            return False
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
