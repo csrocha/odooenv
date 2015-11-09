2a2047e39ae7809fdf991053766ad0f6ef3a1c80
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2015-11-09 18:16:49 -0300

    [VERSION 2.2.10]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='odooenv',
-    version='2.2.9',
+    version='2.2.10',
     author='Cristian S. Rocha',
     author_email='cristian.rocha@moldeo.coop',
     maintainer='Cristian S. Rocha',
